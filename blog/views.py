c412cd3fadbe8c277643b67a1b97e92fa4ed478a
skypwkgamail <skypwk@gmail.com>
2022-03-29 15:09:37 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 
-def index(request):
+class PostList(ListView):
+    model = Post
+    ordering = '-pk';
 
-    posts = Post.objects.all().order_by('-pk')
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts,
-        }
-        
-    )
+
+
+
+    # template_name = 'blog/post_list.html'
+# def index(request):
+#
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts': posts,
+#         }
+#
+#     )
 
 
 def single_post_page(request, pk):
