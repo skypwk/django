4a28d058039103d03db72acfa6fd118ee9d1e513
skypwkgamail <skypwk@gmail.com>
2022-03-29 16:46:48 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- Web_Prj/settings.py
+++ Web_Prj/settings.py
@@ -120,3 +120,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/blog/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
