f6e61735a12fc34c47a265cbb544cbdbf2d69f0b
skypwkgamail <skypwk@gmail.com>
2022-03-30 11:18:21 +0900

    내비게이션 바와 푸터 모듈화하기

--- blog/tests.py
+++ blog/tests.py
@@ -9,6 +9,26 @@ class TestView(TestCase):
     def setUp(self):
         self.client = Client()
 
+
+    def navbar_test(self, soup):
+        navbar = soup.nav
+        self.assertIn('Blog', navbar.text)
+        self.assertIn('About Me', navbar.text)
+
+        logo_btn = navbar.find('a', text='Do It Django')
+        #print(logo_btn)
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
+
+
     def test_post_list(self):
 
         response = self.client.get('/blog/')
@@ -17,10 +37,8 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         #print(soup)
         self.assertEqual(soup.title.text, 'Blog')
-        navbar = soup.nav
-        #print(navbar)
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+
+        self.navbar_test(soup)
 
         self.assertEqual(Post.objects.count(), 0)
         main_area = soup.find('div', id='main-area')
@@ -61,9 +79,7 @@ class TestView(TestCase):
         soup = BeautifulSoup(response.content, 'html.parser')
         #print(soup)
 
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
 
 
         self.assertIn(post_001.title, soup.title.text)
